bbe38fd62ccf569224ae21426063cb0060709186
wagnnermorais <wagnermoraiscnt@gmail.com>
2023-07-07 23:40:56 -0300

    feat: new back end course

---
+++ curso-back-end/conversoes/conversoes.js
@@ -0,0 +1,9 @@
+// conversão implicita
+
+const numero = 456;
+const numeroString = "456";
+
+// Number() converte para um número
+// String() converte para uma string
+
+console.log(numero + Number(numeroString));
