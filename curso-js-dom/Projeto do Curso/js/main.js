758b1bb1da7886ed924524457872c1dba933f500
wagnnermorais <wagnermoraiscnt@gmail.com>
2023-07-16 20:47:19 -0300

    feat: finishing course

--- curso-js-dom/Projeto do Curso/js/main.js
+++ curso-js-dom/Projeto do Curso/js/main.js
@@ -1,37 +1,61 @@
-const subtrair = document.querySelector("#subtrair");
-const somar = document.querySelector("#somar");
-const controle = document.querySelectorAll(".controle-ajuste");
+const controle = document.querySelectorAll("[data-controle]");
+const estatistica = document.querySelectorAll("[data-estatistica]");
+
+const pecas = {
+  bracos: {
+    forca: 29,
+    poder: 35,
+    energia: -21,
+    velocidade: -5,
+  },
+
+  blindagem: {
+    forca: 41,
+    poder: 20,
+    energia: 0,
+    velocidade: -20,
+  },
+  nucleos: {
+    forca: 0,
+    poder: 7,
+    energia: 48,
+    velocidade: -24,
+  },
+  pernas: {
+    forca: 27,
+    poder: 21,
+    energia: -32,
+    velocidade: 42,
+  },
+  foguetes: {
+    forca: 0,
+    poder: 28,
+    energia: 0,
+    velocidade: -2,
+  },
+};
 
 controle.forEach((elemento) => {
   elemento.addEventListener("click", (evento) => {
     manipulaDados(evento.target.textContent, evento.target.parentNode);
+    atualizaEstatistica(evento.target.dataset.peca);
   });
 });
 
 function manipulaDados(operacao, controle) {
-  const peca = controle.querySelector(".controle-contador");
+  const peca = controle.querySelector("[data-contador]");
 
   if (operacao === "-") {
-    peca.value = Number(peca.value) - 1;
+    peca.value = parseInd(peca.value) - 1;
   } else {
-    peca.value = Number(peca.value) + 1;
+    peca.value = parseInt(peca.value) + 1;
   }
 }
 
-// const robotron = document.querySelector("#robotron");
-// // função anônima dentro do event listener
-// robotron.addEventListener("click", function () {
-//   console.log("Cliquei no robo!");
-// });
-
-// // arrow function
-// robotron.addEventListener("click", () => {
-//   console.log("Cliquei no robo com arrow function!");
-// });
-
-// // função nomeada - declaração de função
-// function sayHello(nome) {
-//   console.log(`${nome}, bem-vindo ao Robotron 2000!`);
-// }
-
-// sayHello("Pedro");
+function atualizaEstatistica(peca) {
+  estatistica.forEach((elemento) => {
+    elemento.textContent =
+      parseInt(elemento.textContent) +
+      pecas[peca][elemento.dataset.estatistica];
+  });
+}
